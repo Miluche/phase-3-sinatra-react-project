9751bb7f07ec5ad784f860cef80631f7e020e2df
Miluche <meshkipla@gmail.com>
2023-03-08 20:27:02 +0300

    used endpoints for homepage and search in frontend

--- blog-frontend/src/pages/Home/index.jsx
+++ blog-frontend/src/pages/Home/index.jsx
@@ -3,6 +3,7 @@ import EmptyList from '../../components/common/EmptyList';
 import BlogList from '../../components/Home/BlogList';
 import Header from '../../components/Home/Header';
 import SearchBar from '../../components/Home/SearchBar';
+import {blogList} from "../../config/data";
 // import { blogList } from '../../config/data';
 
 const Home = () => {
@@ -18,20 +19,54 @@ const Home = () => {
   // Search submit
   const handleSearchBar = (e) => {
     e.preventDefault();
-    handleSearchResults();
+    handleSearchResults(searchKey);
   };
 
-  // Search for blog by category
-  const handleSearchResults = () => {
-    const filteredBlogs = blogs.filter((blog) =>
-      blog.category.toLowerCase().includes(searchKey.toLowerCase().trim())
-    );
-    setBlogs(filteredBlogs);
+  // Search for blog using the three endpoints
+  const handleSearchResults = (searchKey) => {
+    if (!searchKey) {
+      // If search key is empty, show all blogs
+      fetch("http://localhost:9292/")
+          .then((r) => r.json())
+          .then((blogs) => setBlogs(blogs));
+    } else {
+      // Search for blogs by category, author name, or created date
+      const categorySearchUrl = `http://localhost:9292/blogs/${searchKey}`;
+      const authorSearchUrl = `http://localhost:9292/blogs/author/${searchKey}`;
+      const dateSearchUrl = `http://localhost:9292/blogs/created_on/${searchKey}`;
+
+      Promise.all([
+        fetch(categorySearchUrl),
+        fetch(authorSearchUrl),
+        fetch(dateSearchUrl),
+      ])
+          .then((responses) =>
+              Promise.all(
+                  responses.map((response) => {
+                    if (response.status === 200) {
+                      return response.json();
+                    }
+                    return null;
+                  })
+              )
+          )
+          .then((blogs) => {
+            // Combine blogs from all endpoints
+            const filteredBlogs = blogs.flat().filter((blog) => blog !== null);
+            setBlogs(filteredBlogs);
+            // console.log(blogs.json());
+          });
+    }
   };
+
+
 
   // Clear search and show all blogs
   const handleClearSearch = () => {
-    setBlogs(blogs);
+    // setBlogs(blogs);
+    fetch("http://localhost:9292/")
+        .then((r) => r.json())
+        .then((blogs)=>setBlogs(blogs));
     setSearchKey('');
   };
 
